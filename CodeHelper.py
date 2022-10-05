f602f57c2d13047e534063509756a7446105ed92
Subhranil-Maity <60032185+Subhranil-Maity@users.noreply.github.com>
2022-10-05 00:34:23 -0700

    added version command improver command scalability and added storage support and not but not the least started making goto function

--- CodeHelper.py
+++ CodeHelper.py
@@ -1,7 +1,28 @@
-import os
+import json
+import os.path
 import sys
 
 __version__ = "0.0.1"
+
+storagePath = "C:\\Code-Helper"
+defaultConfig = {
+
+}
+
+
+def doesStorgeExists() -> bool:
+    """
+    check if storge exists
+    :return: existence of storage directory
+    """
+    return os.path.isdir(storagePath)
+
+
+def TemplatesPath() -> str:
+    """
+    returns path to Templates
+    """
+    return storagePath + "\\templates"
 
 
 def helpCom() -> None:
@@ -15,14 +36,28 @@ def helpCom() -> None:
         print(command['name'] + "\t" + command['dec'])
 
 
+def gotoCom() -> None: pass
+
+
 # Contains data o all the commands and its working
 commands = [
     {
         "name": "help",
         "dec": "Shows all the usable commands",
         "com": helpCom
+    },
+    {
+        "name": "version",
+        "dec": "Shows Version",
+        "com": lambda: print(__version__)
+    },
+    {
+        "name": "goto",
+        "dec": "Brings you to a predefined workspace",
+        "com": gotoCom
     }
 ]
+
 
 def showError(eCode: int, args: list = None) -> None:
     """
@@ -39,16 +74,39 @@ def showError(eCode: int, args: list = None) -> None:
 
 
 def runCom(com) -> dict:
+    """
+    returns the command info or just error
+    :param com: command
+    :return: command's info
+    """
     for command in commands:
-        if com[1] == command['name']: return command
+        if com == command['name']: return command
     return {
         "com": lambda: showError(2, com)
     }
 
 
+def genStorage() -> None:
+    """
+    generates Storage
+    """
+    os.mkdir(storagePath)
+    os.mkdir(TemplatesPath())
+    with open(storagePath + "\\Config.json", "w") as config:
+        config.write(json.dumps(defaultConfig))
+
+
+def sCheck() -> None:
+    """
+    check the storage
+    """
+    if not doesStorgeExists(): genStorage()
+
+
 def app():
+    sCheck()
     argc = len(sys.argv)
     argv = sys.argv
     # print(argc, argv)
     if not argc >= 2: showError(1)
-    runCom(argv)['com']()
+    runCom(argv[1])['com']()
